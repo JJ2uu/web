e554844dce50fc9836e1d0685168cdddafdb3b8a
JJ2uu <124110688+JJ2uu@users.noreply.github.com>
2023-02-02 17:55:51 +0900

    Day 4

---
+++ day04/out.js
@@ -0,0 +1,18 @@
+// 함수는 여러절차가 있는 처리를 이름을 부여하여 기능을 정의!
+function 빨래하다() {
+    console.log ("1. 먼지를 털자.")
+    console.log ("2. 물에 집어 넣는다.")
+    console.log ("3. 비누를 바른다.")
+    console.log ("4. 오물조물 비빈다.")
+    console.log ("5. 물에 여러번 헹군다.")
+    console.log ("6. 물을 짠다.")
+    console.log ("7. 빨랫대에 넌다.")
+}
+//  청소하다에 대한 함수 정의해보기
+function 바닥청소() {
+    console.log ("1. 빗자루질을 먼저 한다.")
+    console.log ("2. 청소기를 돌린다.")
+    console.log ("3. 물걸레로 닦는다.")
+    console.log ("4. 스팀청소기로 한번 더 닦아준다.")
+    console.log ("5. 마른 걸레로 다시 닦아준다.")
+}
